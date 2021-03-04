e67764d224bc2edb352c0216531b9a1991c29a14
diaodiao <775508288@qq.com>
2021-03-04 11:48:20 +0800

    修改了代码

--- idea-git-test/src/com/minemine/Test.java
+++ idea-git-test/src/com/minemine/Test.java
@@ -4,5 +4,10 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello git!");
+        test();
+    }
+
+    public static void test(){
+        System.out.println("hello java!");
     }
 }
